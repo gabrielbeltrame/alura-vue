a64423f4b4e6189f3ef331272e6ab688ae5432b5
Gabriel Beltrame Silva <silvagabrielbeltrame@gmail.com>
2020-02-06 22:50:55 -0300

    diretivas e formulario

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -14,7 +14,7 @@
     <ul class="lista-fotos">
       <li class="lista-fotos-item" v-for="foto of fotosComFiltro" v-bind:key="foto._id">
         <meu-painel :titulo="foto.titulo">
-          <imagem-responsiva :url="foto.url" :titulo="foto.titulo" />
+          <imagem-responsiva v-meu-transform:scale.animate="1.1" :url="foto.url" :titulo="foto.titulo" />
           <meu-botao
             tipo="button"
             rotulo="REMOVER"
